e8992f97d76c45ae76a39cd6e9a639f36756e4cb
ben <x_z_gfree@163.com>
2022-07-30 08:46:28 +0800

    修改 删除部分无用的项目

--- projects/flutter_100_day/android/app/src/main/kotlin/com/example/flutter_100_day/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_100_day
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- projects/flutter_100_day/lib/config.dart
+++
@@ -1,5 +0,0 @@
-class Environment {
-  static const isDevelop = String.fromEnvironment('RUN_ENV') == 'develop';
-  static const isAlpha = String.fromEnvironment('RUN_ENV') == 'alpha';
-  static const isProduction = String.fromEnvironment('RUN_ENV') == 'production';
-}
