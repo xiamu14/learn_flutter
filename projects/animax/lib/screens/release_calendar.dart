e3cd6726a10eb99f3345c607c6b5e8a19fff6324
ben <x_z_gfree@163.com>
2022-08-04 14:09:56 +0800

    feature: develop release calendar page.

--- projects/animax/lib/screens/release_calendar.dart
+++ projects/animax/lib/screens/release_calendar.dart
@@ -2,6 +2,8 @@ import 'package:animax/widgets/custom_app_bar.dart';
 import 'package:animax/widgets/empty.dart';
 import 'package:anime_icons/anime_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:date_utils/date_utils.dart' as utils;
 
 class ReleaseCalendar extends StatefulWidget {
   const ReleaseCalendar({Key? key}) : super(key: key);
@@ -14,15 +16,67 @@ class ReleaseCalendar extends StatefulWidget {
 
 class _ReleaseCalendarState extends State<ReleaseCalendar> {
   final List<String> _data = [];
+  List<DateTime> _calendar = [];
+  late DateTime _selectedDay;
+
+  @override
+  void initState() {
+    super.initState();
+    final now = DateTime.now();
+    final weekStartDay = utils.DateUtils.firstDayOfWeek(now);
+    final List<DateTime> calendarCache = [];
+    for (var i = 0; i < 7; i++) {
+      final day = weekStartDay.add(Duration(days: i));
+      calendarCache.add(day);
+    }
+    setState(() {
+      _calendar = calendarCache;
+      _selectedDay = now;
+    });
+  }
+
+  void onSelectDay(DateTime day) {
+    setState(() {
+      _selectedDay = day;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final sw = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Column(
         children: [
           const CustomAppBar(
             title: 'Release Calendar',
             iconName: AnimeIcons.moreCircle,
+          ),
+          SizedBox(
+            width: sw,
+            height: 80,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: _calendar
+                    .asMap()
+                    .entries
+                    .map(
+                      (entry) => GestureDetector(
+                        onTap: () {
+                          onSelectDay(entry.value);
+                        },
+                        child: CalendarDay(
+                          day: utils.DateUtils.formatDay(entry.value),
+                          dayText: utils.DateUtils.weekdays[entry.key],
+                          isActive: utils.DateUtils.isSameDay(
+                              _selectedDay, entry.value),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
           ),
           Flexible(
             child: Padding(
@@ -44,6 +98,64 @@ class _ReleaseCalendarState extends State<ReleaseCalendar> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class CalendarDay extends StatelessWidget {
+  const CalendarDay({
+    Key? key,
+    this.isActive = false,
+    required this.day,
+    required this.dayText,
+  }) : super(key: key);
+
+  final bool isActive;
+  final String day;
+  final String dayText;
+
+  @override
+  Widget build(BuildContext context) {
+    final secondaryColor = isActive ? Colors.white : const Color(0xffbdbdbd);
+    return Row(
+      children: [
+        Container(
+          width: 50,
+          height: 78,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: isActive ? Theme.of(context).primaryColor : Colors.white,
+            border: isActive
+                ? Border.all(color: Colors.transparent)
+                : Border.all(color: secondaryColor, width: 2),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                dayText,
+                style: GoogleFonts.urbanist(
+                  textStyle: TextStyle(color: secondaryColor),
+                ),
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              Text(
+                day,
+                style: GoogleFonts.urbanist(
+                    textStyle: TextStyle(color: secondaryColor),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+      ],
     );
   }
 }
